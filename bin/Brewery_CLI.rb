0b0dc8148c8e694cb13868f4cfb1f7e4f3379e9c
Thomas Pope <tpope0928@gmail.com>
2018-10-21 22:03:56 +0000

    Scrapper Work

--- bin/Brewery_CLI.rb
+++ bin/Brewery_CLI.rb
@@ -1,3 +1,5 @@
 #!/usr/bin/env ruby
 
 require_relative '../lib/Brewery_CLI'
+
+Scrapper.scrape_data
--- lib/Brewery_CLI/Brewery_CLI.rb
+++ lib/Brewery_CLI/Brewery_CLI.rb
@@ -3,3 +3,26 @@ require 'nokogiri'
 require 'pry'
 require 'rake'
 
+class Brewery
+  attr_accessor :name, :address, :telephone, :url, :brewery_type
+  
+  @all = []
+  
+  def initialize
+    @all << self
+  end
+  
+end
+
+class Scrapper
+  
+  def self.scrape_data
+    doc =
+      Nokogiri::HTML(open("https://www.brewersassociation.org/directories/breweries/"))
+      doc.css(".vcard simple brewery-info").each do |brewery|
+        binding.pry
+      end
+      
+    end
+  end
+end
